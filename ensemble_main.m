c724bcaf0ca7834ec49942e383dc4e2343bd40ff
Victor Suciu <victorsuciu20@gmail.com>
2021-03-10 16:27:42 -0800

    added ensemble model

---
+++ ensemble_main.m
@@ -0,0 +1,74 @@
+function ensemble_main()
+    % make all folders fisible to matlab
+    addpath('cost_functions');
+    addpath('d_cost_functions');
+    addpath('transfer_functions');
+    addpath('d_transfer_functions');
+    addpath('utility');
+    addpath('data');
+    addpath('nn_components');
+    
+    % Training datapoints out of 60,000. The rest are used for validation
+    TRAIN_SIZE = 50000;
+    
+    % READ ALL DATA
+    
+    all_data = readmatrix('train.csv'); % read all 60,000 labeled datapoints and labels into matrix
+    all_submission_data = readmatrix('test.csv'); % read all 10,000 submission datapoints into matrix
+    submission_data = all_submission_data(:, 2:785)' * (1/255); % normalize, and get rid of the useles "id" column in the submission file
+    
+    all_examples = all_data(:, 3:786)' * (1/255); % normalize datapoints
+    disp(all_examples(1:20, 1:10));
+    all_labels = to_one_hot(all_data(:, 2), 0, 9); % convert labels (0-9) to one-hot vectors
+    
+    % split training and validation data
+    train_data = all_examples(:, 1:TRAIN_SIZE);
+    train_labels = all_labels(:, 1:TRAIN_SIZE);
+    test_data = all_examples(:, (TRAIN_SIZE + 1):60000);
+    test_labels = all_labels(:, (TRAIN_SIZE + 1):60000);
+    
+    % hyperparameters
+    epochs = 1;
+    stop_buff = 1;
+    stop_thresh = -1;
+    std = 0.4;
+    batch_size = 32;
+    momentum = 0;
+    lr = 0.05;
+    num_models = 2;
+    
+    ensemble = Ensemble();
+    
+    % build models
+    for i = 1:num_models
+        mlp = MultilayerPerceptron(@cross_entropy, @d_cross_entropy);
+
+        mlp.add_layer(PerceptronLayer(20, 784, @my_tanh, @d_my_tanh, lr, momentum, std));
+        mlp.add_layer(PerceptronLayer(10, 20, @softmax, @d_softmax, lr, momentum, std));
+        
+        ensemble.add_model(mlp);
+    end
+    
+    
+    % train the model
+    [losses, acc, acc_list] = ensemble.fit( ...
+        train_data, ...
+        train_labels, ...
+        epochs, ...
+        batch_size, ...
+        stop_buff, ...
+        stop_thresh, ...
+        test_data, ...
+        test_labels, ...
+        @accuracy ...
+    );
+    
+    % predict labels for submission data
+    final_preds = one_hot_to_int(hardmax(ensemble.frozen_forward(submission_data, test_data, test_labels)), 0, 9);
+    
+    % format matrix for submission
+    submission_matrix = [(60001:70000)' final_preds];
+    
+    % write submission matrix to data/SUBMISSION.csv
+    writematrix(submission_matrix, 'data/SUBMISSION.csv');
+end
---
+++ nn_components/Ensemble.m
@@ -0,0 +1,72 @@
+classdef Ensemble < handle
+    
+    properties
+        model_list
+    end
+    
+    methods
+        function obj = Ensemble()
+            obj.model_list = [];
+        end
+        
+        function add_model(obj, model)
+            % add model to list
+            obj.model_list = [obj.model_list model];
+        end
+        
+        function [losses, best_metrics, all_metrics] = fit(obj, examples, labels, epochs, batch_size, stop_buff, stop_thresh, test_data, test_labels, metric_func)
+            % fit all models in model_list one after another. Also, record
+            % loss, best_metric, and all_metrics for each model in a
+            % matrix, where each row is the result from one model.
+            
+            losses = [];
+            best_metrics = [];
+            all_metrics = [];
+            
+            for i = 1:size(obj.model_list, 2)
+                [curr_losses, curr_best_metrics, curr_all_metrics] = obj.model_list(i).fit( ...
+                    examples, ...
+                    labels, ...
+                    epochs, ...
+                    batch_size, ...
+                    stop_buff, ...
+                    stop_thresh, ...
+                    test_data, ...
+                    test_labels, ...
+                    metric_func ...
+                );
+                losses = [losses; curr_losses];
+                best_metrics = [best_metrics; curr_best_metrics];
+                all_metrics = [all_metrics; curr_all_metrics];
+            end
+            
+            disp('FINAL INDIVIDUAL METRICS');
+            disp(best_metrics);
+            disp('FINAL ENSEMBLE METRIC');
+            disp(metric_func(hardmax(obj.frozen_forward(test_data, test_data, test_labels)), test_labels));
+        end
+        
+        function weighted_votes = frozen_forward(obj, vec, test_data, test_labels)
+            % perform the voting algorithm. The final votes are the average
+            % of the weighted sum of each model's predictions, where the weights are the
+            % model's (precision * recall) score per-class.
+            
+            weighted_votes = zeros(size(test_labels));
+            
+            for i = 1:size(obj.model_list, 2)
+                % get preditioncs
+                preds = obj.model_list(i).frozen_forward(vec);
+                
+                % compute model weights
+                confidence_weights = repmat(compute_prec_rec_weight(hardmax(preds), test_labels), 1, size(vec, 2));
+                
+                % add to the weighted sum
+                weighted_votes = weighted_votes + (confidence_weights .* preds);
+            end
+            
+            % average the weighted sum
+            weighted_votes = weighted_votes * (1 / size(vec, 2));
+        end
+    end
+end
+
