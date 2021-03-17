e4f8ee5d23e17f05f19dfb2b394cb94378390170
Donghee Lee <heedong@uw.edu>
2021-03-16 18:53:35 -0700

    Fix adding models

--- ensemble_main.m
+++ ensemble_main.m
@@ -11,10 +11,6 @@ function ensemble_main()
     
     % READ ALL DATA
     % read data -- CHANGE THE FILE TO YOUR AUGMENTED DATASET
-    aug_data = readmatrix('augmented/augmented_train_2021_03_03_23_56_09.csv'); 
-    aug_label = readmatrix('augmented/augmented_label_2021_03_03_23_56_09.csv');
-    disp(size(aug_label));
-    
     all_submission_data = readmatrix('test.csv'); % read all 10,000 submission datapoints into matrix
     submission_data = all_submission_data(:, 2:785)' * (1/255); % get rid of the useles "id" column in the submission file
     
@@ -27,21 +23,25 @@ function ensemble_main()
     TRAIN_SIZE = 50000;
     valid_data = all_examples(:, (TRAIN_SIZE + 1):60000);
     valid_labels = all_labels(:, (TRAIN_SIZE + 1):60000);
-
     
     ensemble = Ensemble();
     
     % LOAD MODELS 
     % load mlp models from .mat files 
-    load("models/model_2021_3_3_5_14.mat", "mlp");     
-    ensemble.add_model(mlp);
+    model_files = [
+        "models/HansModal_2021_3_12_22_17_METRIC_904.mat"
+        "models/KateModelle_aug_2021_3_11_14_36_METRIC_8926.mat"
+        "models/AdalynModelina_2021_3_12_1_15_METRIC_9014.mat"
+        "models/EdnaMode_2021_3_16_4_26_METRIC_8992.mat"
+        "models/aug_model_2021_3_4_5_9.mat"
+        "models/aug_model_2021_3_7_3_11.mat"];
     
-    load("models/aug_model_2021_3_4_5_9.mat", "mlp");     
-    ensemble.add_model(mlp);
-    
-    load("models/aug_model_2021_3_4_20_35.mat", "mlp");     
-    ensemble.add_model(mlp);
-    
+    for i = 1:length(model_files)
+        load(model_files(i), "mlp");
+        ensemble.add_model(mlp);
+    end
+        
+    disp(accuracy(hardmax(ensemble.frozen_forward(valid_data, valid_data, valid_labels)), valid_labels));
     
     % predict labels for submission data
     final_preds = one_hot_to_int(hardmax(ensemble.frozen_forward(submission_data, valid_data, valid_labels)), 0, 9);
@@ -51,7 +51,7 @@ function ensemble_main()
     
     % write submission matrix 
     t = datetime('now');
-    submission_timestamp = "data/ensemble_submission_" + year(t) + '_' + month(t) + '_' + day(t) + '_' + hour(t) + '_' + minute(t) + '.csv';
+    submission_timestamp = "data/ensemble_with_Edna_" + year(t) + '_' + month(t) + '_' + day(t) + '_' + hour(t) + '_' + minute(t) + '.csv';
     writematrix(submission_matrix, submission_timestamp);
     
 end
